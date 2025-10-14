e6185d62b4fa183beaa98eb2e300479a149dcc53
Arpita Sahoo <arpitasahoo139@gmail.com>
2025-10-14 18:00:37 +0530

    Revert "Merge pull request #2 from Afzal174/Changes"

--- src/routes/users.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { authRequired } from '../middleware/auth.js'
-import { me, updateMe } from '../controllers/userController.js'
-
-const router = Router()
-router.get('/me', authRequired, me)
-router.put('/me', authRequired, updateMe)
-
-export default router
